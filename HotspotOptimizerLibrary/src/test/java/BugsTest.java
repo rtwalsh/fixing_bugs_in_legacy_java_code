5da701aac4e88eb6d59197f8a194b243eda562e0
Robert Walsh <rwalsh@excalibur-solutions.com>
2021-03-17 18:06:33 -0400

    Implement the methods used in the test

--- HotspotOptimizerLibrary/src/test/java/BugsTest.java
+++ HotspotOptimizerLibrary/src/test/java/BugsTest.java
@@ -1,27 +1,37 @@
-import com.assetco.search.results.Asset;
-import com.assetco.search.results.AssetVendor;
-import com.assetco.search.results.AssetVendorRelationshipLevel;
-import com.assetco.search.results.HotspotKey;
+import com.assetco.hotspots.optimization.SearchResultHotspotOptimizer;
+import com.assetco.search.results.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class BugsTest {
+
+    private SearchResults searchResults;
+
+    @BeforeEach
+    public void setUp() {
+        searchResults = new SearchResults();
+    }
 
     @Test
     public void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
         AssetVendor partnerVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
         AssetVendor otherVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
 
-        Asset missing = givenAssetsInResultWithVendor(partnerVendor);
-        Asset disruptingAsset = givenAssetsInResultWithVendor(otherVendor);
+        Asset missing = givenAssetsInResultsWithVendor(partnerVendor);
+        Asset disruptingAsset = givenAssetsInResultsWithVendor(otherVendor);
 
         List<Asset> expected = new ArrayList<Asset>();
-        expected.add(givenAssetsInResultWithVendor(partnerVendor));
-        expected.add(givenAssetsInResultWithVendor(partnerVendor));
-        expected.add(givenAssetsInResultWithVendor(partnerVendor));
-        expected.add(givenAssetsInResultWithVendor(partnerVendor));
+        expected.add(givenAssetsInResultsWithVendor(partnerVendor));
+        expected.add(givenAssetsInResultsWithVendor(partnerVendor));
+        expected.add(givenAssetsInResultsWithVendor(partnerVendor));
+        expected.add(givenAssetsInResultsWithVendor(partnerVendor));
 
         whenOptimize();
 
@@ -30,19 +40,29 @@ public class BugsTest {
     }
 
     private void thenHotspotHasExactly(HotspotKey hotspotKey, List<Asset> expected) {
+        assertEquals(expected, searchResults.getHotspot(hotspotKey).getMembers());
     }
 
     private void thenHotspotDoesNotHave(HotspotKey hotspotKey, Asset asset) {
+        assertFalse(searchResults.getHotspot(hotspotKey).getMembers().contains(asset));
     }
 
     private void whenOptimize() {
+        SearchResultHotspotOptimizer optimzier = new SearchResultHotspotOptimizer();
+        optimzier.optimize(searchResults);
     }
 
-    private Asset givenAssetsInResultWithVendor(AssetVendor partnerVendor) {
-        return null;
+    private Asset givenAssetsInResultsWithVendor(AssetVendor vendor) {
+        BigDecimal zero = new BigDecimal("0.0");
+        AssetPurchaseInfo last30Days = new AssetPurchaseInfo(0, 0, new Money(zero), new Money(zero));
+        AssetPurchaseInfo last24Hours = new AssetPurchaseInfo(0, 0, new Money(zero), new Money(zero));
+        Asset asset = new Asset(null, null, null, null, last30Days, last24Hours, null, vendor);
+        searchResults.addFound(asset);
+        return asset;
     }
 
-    private AssetVendor makeVendor(AssetVendorRelationshipLevel partner) {
-        return null;
+    private AssetVendor makeVendor(AssetVendorRelationshipLevel relationshipLevel) {
+        AssetVendor vendor = new AssetVendor(null, null, relationshipLevel, 0.0f);
+        return vendor;
     }
 }
